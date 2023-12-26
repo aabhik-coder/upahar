6df2d6a553624dafeb81b05b0a5f0a8d1d79ca06
Aabhik <59855005+aabhik-coder@users.noreply.github.com>
2023-12-26 08:11:20 +0545

    added base template and  cateogry page and changed homepage too

--- upahar/store/views.py
+++ upahar/store/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Product
+from .models import Category
 # Create your views here.
 def home(request):
     products = Product.objects.all()
     return render(request,'home.html',{'products':products})
+
+def categories(request):
+    categorys= Category.objects.all();
+    return render(request,'categories.html',{'categorys':categorys})
